d50c8f8d7a4c0621e370e991d64ff517c531fcfd
Keanne <keannemadia90@gmail.com>
2026-05-02 18:44:48 +0800

    Database

--- routes.php
+++ routes.php
@@ -1,12 +1,5 @@
 <?php
 
-// return [
-//     '/' => 'controllers/home.php',
-//     'listings' => 'controllers/listings/index.php',
-//     'listings/create' => 'controllers/listings/create.php',
-//     '404' => 'controllers/error/404.php',
-// ];
-
-$router->get('/', 'Controllers/home.php');
-$router->get('/listings', 'Controllers/listings/index.php');
-$router->get('/listings/create', 'Controllers/listings/create.php');
+$router->get('/', 'controllers/home.php');
+$router->get('/listings', 'controllers/listings/index.php');
+$router->get('/listings/create', 'controllers/listings/create.php');
